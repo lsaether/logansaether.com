0fea3a6226dfa8b8880d6c0a802f4ce56bfc6897
Logan Saether <x@logansaether.com>
2019-10-14 00:12:44 +0200

    Updates

---
+++ precache-manifest.c8cb2ab99961f87ec901682429fd2c64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16a8aa76b392eba7268e8e330ab5d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e19ab6035acea9fe737",
+    "url": "/static/css/main.b4e8c2a7.chunk.css"
+  },
+  {
+    "revision": "eec50518cc665c501650",
+    "url": "/static/js/2.483ee823.chunk.js"
+  },
+  {
+    "revision": "9e19ab6035acea9fe737",
+    "url": "/static/js/main.a3c8e6ca.chunk.js"
+  },
+  {
+    "revision": "4c37846696bcf0b90563",
+    "url": "/static/js/runtime-main.33159e11.js"
+  },
+  {
+    "revision": "bb5f03dff006bf4364c0065b53797de2",
+    "url": "/static/media/logan_nobg.bb5f03df.svg"
+  }
+]);
